5842aef1f4b4c3e9146115a0878a797a4d05594b
Will Holcomb <dys@dhappy.org>
2021-05-16 22:50:00 -0400

    pinnin' CBORs 🏐

--- src/About.jsx
+++ src/About.jsx
@@ -25,10 +25,11 @@ export default () => (
       <Text mt={5}>Examples:</Text>
       <UnorderedList>
         <ListItem><Link to="/build/bafyreiadbcuiiwmdhzca2ajxwjgguwjyidujex2ptfu77rpidyt65lsm5e">Gazoo</Link> <i>(~20 CBOR nodes)</i></ListItem>
-        <ListItem><Link to="/build/bafyreibu2ase2fvphe47yjfiebtvvx236f4pbgqzgraayyd2bfszekldzi">MetaGame Flag</Link> <i>(~30 CBOR nodes)</i></ListItem>
+        <ListItem><Link to="/build/bafyreigrpczkdehmrc4t3fl64amaelrxdogh4femcu6j6g43kyvorqz7cq">MetaGame Flag</Link> <i>(~30 CBOR nodes)</i></ListItem>
         <ListItem><Link to="/build/ceramic%3A%2F%2Fk3y52l7qbv1fryb0xtwadtq0z31win1rgiux03gme0t6ixa6oryayfimfohotv7y8">MetaGame Flag</Link> <i>(~30 Ceramic nodes)</i></ListItem>
         <ListItem><Link to="/build/bafyreif645kruyhewydqwyjpor6z24mu3dzj2jsi6f2w57xoxcmnlf4ypa">Linux Fox</Link> <i>(~150 CBOR nodes)</i></ListItem>
-        <ListItem><Link to="/build/bafyreihqodbfy47utwfqyxnmsxpxgs5p3g5opcqnilor6flt3lwhksxrsu">Chakras</Link> <i>(1000+ CBOR nodes)</i></ListItem>
+        <ListItem><Link to="/build/bafyreiepdlskoh2ogf63cfbjccjpz7xtporqwoo4sna77tz3modfttu7p4">Chakras</Link> <i>(1000+ CBOR nodes)</i></ListItem>
+        <ListItem><Link to="/build/bafyreifnikp6jlpzph7hqwwpz6wa6lqoos5ky45j56ivdv3cgrt7joc7gy">Income Distribution in 2007</Link></ListItem>
       </UnorderedList>
     </Flex>
   </Flex>
